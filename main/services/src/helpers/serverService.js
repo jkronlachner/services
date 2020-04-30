c08dae29ae9e2a1c923568d9910626d67ddcb6ad
paulkrenn <paul.krenn@lab73.at>
2020-04-30 12:44:17 +0200

    Added the serverService.js

---
+++ main/services/src/helpers/serverService.js
@@ -0,0 +1,72 @@
+export const serverService = {
+    getAllServices,
+    addService,
+    changeService,
+    getSpecificService
+};
+
+function getAllServices() {
+    const Http = new XMLHttpRequest();
+    const url = "http://localhost:8080/api/services";
+    Http.open("GET", url);
+    Http.send();
+    return new Promise(resolve => {
+        Http.onreadystatechange = (e) => {
+            if (Http.readyState === 4) {
+                let response = Http.responseText;
+                let array = JSON.parse(response);
+                resolve(array);
+            }
+        }
+    })
+}
+
+function getSpecificService(id) {
+    const Http = new XMLHttpRequest();
+    const url = "http://localhost:8080/api/services/" + id;
+    Http.open("GET", url);
+    Http.send();
+    return new Promise(resolve => {
+        Http.onreadystatechange = (e) => {
+            if (Http.readyState === 4) {
+                let response = Http.responseText;
+                let object = JSON.parse(response);
+                resolve(object);
+            }
+        }
+    })
+}
+
+function changeService(id, object) {
+    const Http = new XMLHttpRequest();
+    const url = "http://localhost:8080/api/services/" + id;
+    Http.open("PUT", url);
+    Http.setRequestHeader("Content-Type", "application/json");
+    Http.send(JSON.stringify(object));
+    return new Promise(resolve => {
+        Http.onreadystatechange = (e) => {
+            if (Http.readyState === 4) {
+                let response = Http.responseText;
+                let object = JSON.parse(response);
+                resolve(object);
+            }
+        }
+    })
+}
+
+function addService(object) {
+    const Http = new XMLHttpRequest();
+    const url = "http://localhost:8080/api/services";
+    Http.open("POST", url);
+    Http.setRequestHeader("Content-Type", "application/json");
+    Http.send(JSON.stringify(object));
+    return new Promise(resolve => {
+        Http.onreadystatechange = (e) => {
+            if (Http.readyState === 4) {
+                let response = Http.responseText;
+                let object = JSON.parse(response);
+                resolve(object);
+            }
+        }
+    })
+}
