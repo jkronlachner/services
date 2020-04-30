0f44cbefb9e2d29bc34f7023f02d7ccf7b9df0c3
davidgangl <david.gangl@lab73.at>
2020-04-30 11:20:14 +0200

    Components: - listItemComponent

--- main/services/src/components/listComponent.js
+++ main/services/src/components/listComponent.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import {ListItemComponent} from "./listItemComponent";
 
 let marginIndicator = 25;
 const useStyles = makeStyles(theme => ({
@@ -42,7 +42,17 @@ const useStyles = makeStyles(theme => ({
 export function ListComponent() {
     const classes = useStyles();
 
-    const [value, setValue] = React.useState(2);
+    const [value, setValue] = useState(2);
+    const [services, setServices] = useState([{
+        "id": 0,
+        "name": "Putzen",
+        "date": "2019-03-08",
+        "lat": "45",
+        "lon": "13",
+        "employed": 1
+    }]);
+
+
 
     const handleChange = (event, newValue) => {
         console.log(newValue)
@@ -61,6 +71,7 @@ export function ListComponent() {
         console.log(marginIndicator);
         setValue(newValue);
     };
+
     return <div className={classes.root}>
         <h1 style={{flexGrow: 1}}>Discover</h1>
         {
@@ -91,7 +102,7 @@ export function ListComponent() {
                          label="best"/>
                     <Tab classes={{root: classes.tab}} style={{textTransform: "lowercase", fontSize: 20}} label="cheapest"/>
                     <Tab classes={{root: classes.tab}} style={{textTransform: "lowercase", fontSize: 20}} label="rarest"/>
-                </Tabs> :  <Tabs
+                </Tabs> : <Tabs
                     style={{flexGrow: 1, marginBottom: "2rem"}}
                     classes={{indicator: classes.indicator2}}
                     value={value}
@@ -99,12 +110,19 @@ export function ListComponent() {
                     textColor="primary"
                     onChange={handleChange}
                     aria-label="disabled tabs example">
-                    <Tab variant={"h1"} classes={{root: classes.tab}} style={{textTransform: "lowercase", fontSize: 20}} label="best"/>
+                    <Tab variant={"h1"} classes={{root: classes.tab}} style={{textTransform: "lowercase", fontSize: 20}}
+                         label="best"/>
                     <Tab classes={{root: classes.tab}} style={{textTransform: "lowercase", fontSize: 20}} label="cheapest"/>
                     <Tab classes={{root: classes.tab}} style={{textTransform: "lowercase", fontSize: 20}} label="rarest"/>
                 </Tabs>
         }
-        <List style={{flexGrow: 1000000000, backgroundColor: "blue"}}>
+        <List style={{flexGrow: 1000000000}}>
+            {
+                services.map(x => {
+                    console.log("!!!!");
+                    return <ListItemComponent service={x} employee={"Paul Wiesinger"}/>
+                })
+            }
         </List>
     </div>
 }
---
+++ main/services/src/components/listItemComponent.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper"
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import CreateIcon from '@material-ui/icons/Create';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        width: "100wh",
+        display: "flex",
+        alignItems: "center",
+    },
+    column: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    row: {
+        display: "flex",
+        color: theme.palette.primary.main,
+    },
+    icon: {
+        color: theme.palette.secondary.main
+    },
+    emoji: {
+       padding: "1rem"
+    },
+    date: {
+        color: "gray"
+    }
+
+}));
+
+/*"id": 0,
+    "name": "Putzen",
+    "date": "2019-03-08",
+    "lat": "45",
+    "lon": "13",
+    "employed": 1*/
+
+export function ListItemComponent({service, employee}) {
+    const classes = useStyles();
+
+    return <Paper className={classes.container}>
+        <div className={classes.emoji}>😉</div>
+        <div className={classes.column} style={{flex: 1}}>
+            <div>{service.name}</div>
+            <div className={classes.date}>{employee}, {service.date}</div>
+        </div>
+        <div className={classes.row} style={{flex: 1, textAlign: "center"}}>
+            <div style={{marginLeft: "1rem"}}>100</div>
+            <FavoriteIcon/>
+            <div style={{marginLeft: "2rem"}}>250€/h</div>
+        </div>
+        <CreateIcon className={classes.icon} style={{marginRight: "1rem"}}/>
+        <DeleteForeverIcon className={classes.icon} style={{marginRight: "1rem"}}/>
+    </Paper>
+}
